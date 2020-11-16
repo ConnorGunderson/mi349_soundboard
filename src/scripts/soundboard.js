b3ed06277ae61acc8051690fa638f5b43607f4ab
ConnorGunderson <gundydev@gmail.com>
2020-11-15 22:21:34 -0500

    polished animations

--- src/scripts/soundboard.js
+++ src/scripts/soundboard.js
@@ -1,5 +1,15 @@
+let audio;
+
 soundboard.onclick = e => {
+    // if a button is not clicked, return
     if (e.target.nodeName !== 'BUTTON') return;
-    
-    e.target.querySelector("audio").play()
+
+    // init audio object
+    audio = e.target.querySelector("audio");
+
+    // reset audio if not finished yet
+    if (audio.ended === false) audio.load();
+
+    // play the audio
+    audio.play()
 }
